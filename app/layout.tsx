5042f94df5fc8d44e61abcd53217454d12834e4d
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-12-17 11:28:18 +0800

    add: gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -24,12 +25,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GTM_ID = process.env.gtm_id;
   return (
     <html lang="en">
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased relative selection:bg-tech selection:text-white`}
         suppressHydrationWarning
       >
+        {GTM_ID ? (
+          <Script id="gtm-loader" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `(
+              function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              }
+            )(window,document,'script','dataLayer','${GTM_ID}');`
+          }} />
+        ) : null}
+        {GTM_ID ? (
+          <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+          </noscript>
+        ) : null}
         {children}
       </body>
     </html>
